318f95cde976e9ca6a0e657219ae11b9dd1f0190
Tushar Budhwani <tusharbudhwani@gmail.com>
2023-10-24 22:53:04 -0400

    Added more test cases for mutation analysis and performed mutation analysis

--- test_suit/test_mutationAdequate.py
+++ test_suit/test_mutationAdequate.py
@@ -15,7 +15,28 @@ class TestTriangleClassification(unittest.TestCase):
         actual = Triangle.classify(15, 32, 34)
         expected = Triangle.Type.SCALENE
         self.assertEqual(actual, expected)
+        
+        #3
+        actual = Triangle.classify(7, 10, 5)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
 
+        #4
+        actual = Triangle.classify(9, 40, 41)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+        
+        #3
+        actual = Triangle.classify(7, 10, 5)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+
+        #4
+        actual = Triangle.classify(9, 40, 41)
+        expected = Triangle.Type.SCALENE
+        self.assertEqual(actual, expected)
+        
+        
         # Invalid triangles
 
         #1 - all sides 0 is an invalid triangle
@@ -77,12 +98,117 @@ class TestTriangleClassification(unittest.TestCase):
         actual = Triangle.classify(-5, 5, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
+        
+        #13 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(2.5, 5, 10)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #14 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(1, 2, 3)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #15 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(3, 1, 2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+         #16 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(2, 3, 1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+         #17 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(1, 1, 2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        # 18 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(1, 2, 1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #19 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(2, 1, 1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        # 20 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(1, 2, 1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #21 - inputs can not be negative and zero
+        actual = Triangle.classify(-1, -2, 0)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #22 - inputs can not be negative and zero
+        actual = Triangle.classify(0, -1, -1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #23 - inputs can not be negative and zero
+        actual = Triangle.classify(-1, 0, -1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #24 - inputs can not be negative and zero
+        actual = Triangle.classify(-1, 2, 0)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #25 - inputs can not be negative and zero
+        actual = Triangle.classify(0, -1, 1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #26 - inputs can not be negative and zero
+        actual = Triangle.classify(1, 0, -1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #26 - inputs can not be negative and zero
+        actual = Triangle.classify(-1, -5, -1)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #27 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(10, 2, 4)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #28 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(2, 10, 4)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+        
+        #29 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        actual = Triangle.classify(4, 2, 10)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
 
         
         # Equilateral triangles
 
         #1
         actual = Triangle.classify(5, 5, 5)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+        
+         #2
+        actual = Triangle.classify(2.5, 2.5, 2.5)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+        
+        #3
+        actual = Triangle.classify(1, 1, 1)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+        
+         #4
+        actual = Triangle.classify(100, 100, 100)
         expected = Triangle.Type.EQUILATERAL
         self.assertEqual(actual, expected)
         
@@ -104,7 +230,22 @@ class TestTriangleClassification(unittest.TestCase):
         actual = Triangle.classify(2, 3, 3)
         expected = Triangle.Type.ISOSCELES
         self.assertEqual(actual, expected)
+        
+        #4
+        actual = Triangle.classify(3, 3, 5)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
 
+        #5
+        actual = Triangle.classify(3, 5, 3)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+
+        #6
+        actual = Triangle.classify(5, 3, 3)
+        expected = Triangle.Type.ISOSCELES
+        self.assertEqual(actual, expected)
+        
 
 if __name__ == '__main__':
     unittest.main()
