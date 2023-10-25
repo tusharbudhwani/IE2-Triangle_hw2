b6b5a851aa415e44c7cb86b68782ab4bf4fcbaf1
Tushar Budhwani <tusharbudhwani@gmail.com>
2023-10-24 21:16:30 -0400

    Enhance Statement Coverage test by adding more test cases

--- test_suit/test_statementCoverage.py
+++ test_suit/test_statementCoverage.py
@@ -18,33 +18,47 @@ class TestTriangleClassification(unittest.TestCase):
 
         # Invalid triangles
 
-        #1 - all sizes 0 is an invalid triangle
+        #1 - all sides 0 is an invalid triangle
         actual = Triangle.classify(0, 0, 0)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-        #2 - Does not follow [(a+b > c), (b+c > a), (c+a > b)] and one size is 0
+        #2 - having a negative side is an invalid triangle
+        actual = Triangle.classify(-5, 2, 6)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+        #3 - having a negative side is an invalid triangle
+        actual = Triangle.classify(2, -5, 6)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+        #4 - having a negative side is an invalid triangle
+        actual = Triangle.classify(2, 6, -5)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
+
+        #5 - Does not follow [(a+b > c), (b+c > a), (c+a > b)] and one size is 0
         actual = Triangle.classify(0, 4, 6)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-        #3 - Does not follow [(a+b > c), (b+c > a), (c+a > b)] and one size is 0
+        #6 - Does not follow [(a+b > c), (b+c > a), (c+a > b)] and one size is 0
         actual = Triangle.classify(6, 0, 4)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-        #4 - Does not follow [(a+b > c), (b+c > a), (c+a > b)] and one size is 0
+        #7 - Does not follow [(a+b > c), (b+c > a), (c+a > b)] and one size is 0
         actual = Triangle.classify(12, 8, 0)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-        #5 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        #8 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
         actual = Triangle.classify(1, 4, 5)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-
-        #6 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
+        #9 - Does not follow [(a+b > c), (b+c > a), (c+a > b)]
         actual = Triangle.classify(5, 5, 10)
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
